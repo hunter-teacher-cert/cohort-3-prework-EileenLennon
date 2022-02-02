ba7ab290d9c1ef50e4a683b59e260be0e89a6a19
Eileen Lennon <96425424+EileenLennon@users.noreply.github.com>
2022-02-02 23:12:13 +0000

    Added pre02 folder

---
+++ pre02/Date.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Date {
+    public static void main(String[] args){
+
+int day = Wednesday
+int date = 02;
+int month = February;
+int year = 2022;
+System.out.print("The current day is ");
+System.out.println(day);
+System.out.print("The current date is ");
+System.out.println("date");
+System.out.print("The current month is ");
+System.out.println(month);
+System.out.print("The current year is ");
+System.out.print("year");
+System.out.println(".");;
+    }
+}
